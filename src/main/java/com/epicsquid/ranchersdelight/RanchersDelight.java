802d228c56902d6dbe7112cefa65e91fd9b39114
EpicSquid <epicsquid315@gmail.com>
2022-08-21 16:52:50 +1000

    Added truffle

--- src/main/java/com/epicsquid/ranchersdelight/RanchersDelight.java
+++ src/main/java/com/epicsquid/ranchersdelight/RanchersDelight.java
@@ -1,11 +1,16 @@
 package com.epicsquid.ranchersdelight;
 
+import com.epicsquid.ranchersdelight.init.RanchersDelightItems;
 import com.tterrag.registrate.Registrate;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import javax.annotation.Nonnull;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(RanchersDelight.MODID)
@@ -14,14 +19,28 @@ public class RanchersDelight {
 
 	private static final Lazy<Registrate> REGISTRATE = Lazy.of(() -> Registrate.create(MODID));
 
+	public static final CreativeModeTab ITEM_GROUP = new CreativeModeTab(RanchersDelight.MODID) {
+		@Override
+		@Nonnull
+		public ItemStack makeIcon() {
+			return new ItemStack(RanchersDelightItems.TRUFFLE.get());
+		}
+	};
+
 	public RanchersDelight() {
 		// Register the setup method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		RanchersDelightItems.init();
 
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	private void setup(final FMLCommonSetupEvent event) {
+	}
+
+	public static Registrate registrate() {
+		return REGISTRATE.get();
 	}
 }
---
+++ src/main/java/com/epicsquid/ranchersdelight/init/RanchersDelightItems.java
@@ -0,0 +1,16 @@
+package com.epicsquid.ranchersdelight.init;
+
+import com.epicsquid.ranchersdelight.RanchersDelight;
+import com.tterrag.registrate.Registrate;
+import com.tterrag.registrate.util.entry.RegistryEntry;
+import net.minecraft.world.item.Item;
+
+public class RanchersDelightItems {
+
+	private static final Registrate REGISTRATE = RanchersDelight.registrate();
+
+	public static final RegistryEntry<Item> TRUFFLE = REGISTRATE.item("truffle", Item::new).tab(() -> RanchersDelight.ITEM_GROUP).register();
+
+	public static void init() {
+	}
+}
